92550ac6dec65ca41751df169e6930edcfe7f9b8
chsevero <chsevero@outlook.com.br>
2021-07-10 18:10:55 -0300

    models bug corrections and add constrains

--- scheduleApp/admin.py
+++ scheduleApp/admin.py
@@ -8,12 +8,26 @@ class AddressInline(admin.StackedInline):
     extra = 1
 
 class PlaceAdmin(admin.ModelAdmin):
-    
     inlines = [AddressInline]
     list_display = ('name',)
 
+    def has_delete_permission(self, request, obj=None):
+        return False
 
-admin.site.register(Doctor)
+class Day_OfAdmin(admin.ModelAdmin):
+    fields = ['doctor', 'day_of']
+    list_display = ('doctor', 'day_of')
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'admission_date', 'active')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'place', 'date')
+
+admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(Place, PlaceAdmin)
-admin.site.register(Day_Of)
-admin.site.register(Schedule)
+admin.site.register(Day_Of, Day_OfAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
--- scheduleApp/models.py
+++ scheduleApp/models.py
@@ -1,52 +1,101 @@
+import datetime
 from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
+from django.core.exceptions import ValidationError
+
 
 class Doctor(models.Model):
-    firstname = models.CharField('Nome', max_length=200)
-    lastname = models.CharField('Sobrenome', max_length=200)
-    admisson_date = models.DateField('Data de Admissão')
+    firstname = models.CharField('First Name', max_length=200)
+    lastname = models.CharField('Last Name', max_length=200)
+    admission_date = models.DateField('Admission Date')
     active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+
 class Place(models.Model):
-    name = models.CharField('Nome', max_length=200)
-    active = models.BooleanField('Ativo', default=True)
+    name = models.CharField('Name', max_length=200)
+    active = models.BooleanField( default=True)
+    def __str__(self):
+        return self.name
 
 class Address(models.Model):
-    country = models.CharField('País', max_length=200)
-    state = models.CharField('Estado', max_length=200)
-    city = models.CharField('Cidade', max_length=200)
-    district = models.CharField('Bairro', max_length=200)
-    street = models.CharField('Rua', max_length=200)
-    number = models.IntegerField('Numero')
+    country = models.CharField('Country', max_length=200)
+    state = models.CharField('State', max_length=200)
+    city = models.CharField('City', max_length=200)
+    district = models.CharField('district', max_length=200)
+    street = models.CharField('Street', max_length=200)
+    number = models.IntegerField('Number')
     place = models.OneToOneField(Place, on_delete=models.CASCADE)
 
 
 class Day_Of(models.Model):
-    MONDAY = "seg"
-    TUESDAY = "ter"
-    WEDNESDAY = "qua"
-    THURSDAY = "qui"
-    FRIDAY = "sex"
-    SATURDAY = "sab"
-    SUNDAY = "dom"
+    class Meta:
+        verbose_name = ("Day Of")
+    SUNDAY = 1
+    MONDAY = 2
+    TUESDAY = 3
+    WEDNESDAY = 4
+    THURSDAY = 5
+    FRIDAY = 6
+    SATURDAY = 7
+    
 
     WEEKDAYS = [
-        (MONDAY, 'Segunda'),
-        (TUESDAY, 'Terça'),
-        (WEDNESDAY, 'Quarta'),
-        (THURSDAY, 'Quinta'),
-        (FRIDAY, 'Sexta'),
-        (SATURDAY, 'Sabado'),
-        (SUNDAY, 'Domingo'),
+        (MONDAY, 'Monday'),
+        (TUESDAY, 'Tuesday'),
+        (WEDNESDAY, 'Wednesday'),
+        (THURSDAY, 'Thursday'),
+        (FRIDAY, 'Friday'),
+        (SATURDAY, 'Saturday'),
+        (SUNDAY, 'Sunday'),
     ]
 
-    day_of = models.CharField("Dia de folga", max_length=15, choices=WEEKDAYS)
-    doctor = models.OneToOneField(Doctor, on_delete=CASCADE)
+    day_of = models.IntegerField("Day Of",choices=WEEKDAYS)
+    doctor = models.ForeignKey(Doctor, on_delete=CASCADE)
+
+    class Meta:
+        unique_together = ['day_of', 'doctor']
+    def __str__(self):
+        return f"{self.day_of}"
+
+    def clean(self):
+        if Schedule.objects.filter(doctor=self.doctor, date__week_day=self.day_of):
+            raise ValidationError('This date has an schedule')
+        return super().clean()
 
 class Schedule(models.Model):
     date = models.DateField('Date')
-    place = models.OneToOneField(Place, on_delete=CASCADE)
-    doctor = models.OneToOneField(Doctor, on_delete=CASCADE)
-
+    place = models.ForeignKey(Place, on_delete=CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=CASCADE)
     
+    class Meta:
+        unique_together = ('date', 'doctor')
+
+    def __str__(self):
+        return f"{self.doctor} {self.place} {self.date}"
+
+    def convert_weekday(self):
+        if self.date.weekday() == 0: return 2
+        if self.date.weekday() == 1: return 3
+        if self.date.weekday() == 2: return 4
+        if self.date.weekday() == 3: return 5
+        if self.date.weekday() == 4: return 6
+        if self.date.weekday() == 5: return 7
+        if self.date.weekday() == 6: return 1
+    
+    def clean(self):
+
+        if Day_Of.objects.filter(doctor=self.doctor, day_of = self.convert_weekday()).exists():
+            print(self.date.weekday())
+            raise ValidationError("This is a day of")
+        return super().clean()
+
+    def save(self):
+        print(self.date.weekday())
+        
+        return super().save()
+        
+        
